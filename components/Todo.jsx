5c2fde7606291ba12442c2e90d310c4c085553db
cpk0709 <cpk0709@gmail.com>
2022-08-05 17:39:27 +0900

    edit Todo.jsx

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useAppDispatch } from "../feature/configureStore";
+import { fetchGetTodoList } from "../feature/modules/todoSlice";
+import { useQuery } from "@tanstack/react-query";
 
 const Todo = () => {
+  const appDispatch = useAppDispatch();
   const todoList = useSelector((store) => store.todo.todo.list);
+
+  // useEffect(() => {
+  //   appDispatch(fetchGetTodoList());
+  // }, [appDispatch]);
+
+  const { data } = useQuery(["todo"], appDispatch(fetchGetTodoList()));
+  console.log(data);
 
   return (
     <div>
