25aaf5b436309ed2e504cb2d8e2025c490ef414d
cpk0709 <cpk0709@gmail.com>
2022-08-05 14:21:38 +0900

    add initialState, reducer

--- feature/modules/todoSlice.js
+++ feature/modules/todoSlice.js
@@ -1,1 +1,14 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  todoId: 0,
+  todoTitle: "",
+  todoContent: "",
+  status: "",
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {},
+});
