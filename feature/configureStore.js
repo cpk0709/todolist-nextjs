33d74a8c3fc86898648d4c5546bcfe36388a1ca6
cpk0709 <cpk0709@gmail.com>
2022-08-05 15:01:22 +0900

    export useDispatch from configureStore

--- feature/configureStore.js
+++ feature/configureStore.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { todoSlice } from "./modules/todoSlice";
+import { useDispatch } from "react-redux";
 
 export const store = configureStore({
   reducer: {
     todo: todoSlice,
   },
 });
+
+export const useAppDispatch = () => useDispatch();
 
 export default store;
