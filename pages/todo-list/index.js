57768341dd508de021699e4a31bc9e6022dbd2e3
cpk0709 <cpk0709@gmail.com>
2022-08-05 15:54:51 +0900

    conntect addTodo middleware

--- pages/todo-list/index.js
+++ pages/todo-list/index.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { useAppDispatch } from "../../feature/configureStore";
+import { fetchAddTodo } from "../../feature/modules/todoSlice";
+
 const TodoListPage = () => {
+  const appDispatch = useAppDispatch();
+
+  const [todo, setTodo] = useState("");
+
+  const getAddTodoForm = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    appDispatch(fetchAddTodo());
+  };
+
   return (
     <div>
       <h1>The Todo List Page</h1>
       <hr />
-      <input type="text" placeholder="Todo 입력" />
+      <input type="text" placeholder="Todo 입력" onChange={getAddTodoForm} />
+      <button onClick={handleAddTodo}>Add Todo</button>
     </div>
   );
 };
