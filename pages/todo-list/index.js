4fe3158a6ada3e2761e40a1c080306ce098c94a2
cpk0709 <cpk0709@gmail.com>
2022-08-05 14:48:42 +0900

    add todo input tag

--- pages/todo-list/index.js
+++ pages/todo-list/index.js
@@ -1,9 +1,11 @@
 const TodoListPage = () => {
-    return (
-        <div>
-            <h1>The Todo List Page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>The Todo List Page</h1>
+      <hr />
+      <input type="text" placeholder="Todo 입력" />
+    </div>
+  );
 };
 
 export default TodoListPage;
