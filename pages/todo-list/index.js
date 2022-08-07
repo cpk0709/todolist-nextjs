6cf9d844f726c020f573a546b7177817daaf2981
cpk <cpk0709@gmail.com>
2022-08-07 23:11:13 +0900

    index 함수 변경

--- pages/todo-list/index.js
+++ pages/todo-list/index.js
@@ -7,16 +7,38 @@ import { Box } from "@chakra-ui/react";
 const TodoListPage = () => {
   const appDispatch = useAppDispatch();
 
-  const [todo, setTodo] = useState("");
+  // const [todo, setTodo] = useState("");
 
-  const getAddTodoForm = (e) => {
-    setTodo(e.target.value);
+  const [todoForm, setTodoForm] = useState({
+    title: "",
+    content: "",
+  });
+
+  const { title, content } = todoForm;
+
+  // const getAddTodoForm = (e) => {
+  //   setTodo(e.target.value);
+  // };
+
+  const onChange = (e) => {
+    console.log("!");
+    console.log(e.target.value);
+    console.log(e.target.name);
+    const nextForm = {
+      ...todoForm,
+      [e.target.name]: e.target.value,
+    };
+    setTodoForm(nextForm);
   };
 
   const handleAddTodo = () => {
-    if (!todo) return;
-    appDispatch(fetchAddTodo(todo));
-    setTodo("");
+    if (!todoForm.title && !todoForm.content) return;
+    appDispatch(fetchAddTodo(todoForm));
+    // setTodo("");
+    setTodoForm({
+      title: "",
+      content: "",
+    });
   };
 
   return (
@@ -26,9 +48,18 @@ const TodoListPage = () => {
       <Box mb="20px">
         <input
           type="text"
-          placeholder="Todo 입력"
-          value={todo}
-          onChange={getAddTodoForm}
+          name="title"
+          placeholder="제목 입력"
+          value={title}
+          onChange={onChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="content"
+          placeholder="내용 입력"
+          value={content}
+          onChange={onChange}
         />
         <button onClick={handleAddTodo}>Add Todo</button>
       </Box>
