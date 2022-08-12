509426dcf9bc6efb59abd4769cfe5cfe177c4fbb
cpk0709 <cpk0709@gmail.com>
2022-08-12 18:02:53 +0900

    TodoListContainer.jsx 코드 입력

---
+++ containers/todo-list/TodoListContainer.jsx
@@ -0,0 +1,50 @@
+import TodoList from "./TodoList";
+import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useMutateAddTodo } from "../../api/todo/todo-hooks";
+
+const TodoListContainer = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: addTodoMutate } = useMutateAddTodo({
+    onSuccess: () => {
+      console.log("success!!");
+      queryClient.invalidateQueries("todo");
+      setTodo("");
+    },
+    onError: (error) => {
+      console.log(error.message);
+      console.log(error);
+    },
+  });
+
+  const [todo, setTodo] = useState("");
+
+  const setTodoForm = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const handleAddTodoFetch = () => {
+    if (!todo) {
+      alert("Todo를 입력해주세요");
+      return;
+    }
+    addTodoMutate({
+      id: Math.random(),
+      todo: todo,
+      status: false,
+    });
+  };
+
+  return (
+    <>
+      <TodoList
+        todo={todo}
+        setTodoForm={setTodoForm}
+        handleAddTodoFetch={handleAddTodoFetch}
+      />
+    </>
+  );
+};
+
+export default TodoListContainer;
