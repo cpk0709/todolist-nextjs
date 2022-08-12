2511ecdd56bab8aad8f642ff6615a0f1bd35ef49
cpk0709 <cpk0709@gmail.com>
2022-08-12 18:02:34 +0900

    todo-list/index.js 내용 수정

--- pages/todo-list/index.jsx
+++ pages/todo-list/index.jsx
@@ -1,93 +1,60 @@
-import { useState } from "react";
-// import { useAppDispatch } from "../../feature/configureStore";
-// import { fetchAddTodo } from "../../feature/modules/todoSlice";
-// import axios from "axios";
-import { useQueryClient } from "@tanstack/react-query";
-import Todo from "../../components/Todo";
-import { Box } from "@chakra-ui/react";
-import { useMutateAddTodo } from "../../api/todo/todo-hooks";
+// import { useState } from "react";
+// import { useQueryClient } from "@tanstack/react-query";
+// import Todo from "../../components/Todo";
+// import { Box } from "@chakra-ui/react";
+// import { useMutateAddTodo } from "../../api/todo/todo-hooks";
 
-const TodoListPage = () => {
-  // const appDispatch = useAppDispatch();
+// const TodoListPage = () => {
+//   const queryClient = useQueryClient();
 
-  const queryClient = useQueryClient();
+//   const { mutate: addTodoMutate } = useMutateAddTodo({
+//     onSuccess: () => {
+//       console.log("success!");
+//       queryClient.invalidateQueries("todo");
+//       setTodo("");
+//     },
+//     onError: (error) => {
+//       console.log(error.message);
+//       console.log(error);
+//     },
+//   });
 
-  const { mutate: addTodoMutate } = useMutateAddTodo({
-    onSuccess: () => {
-      console.log("success!");
-      queryClient.invalidateQueries("todo");
-      setTodo("");
-    },
-    onError: (error) => {
-      console.log(error.message);
-      console.log(error);
-    },
-  });
+//   const [todo, setTodo] = useState("");
 
-  const [todo, setTodo] = useState("");
+//   const getAddTodoForm = (e) => {
+//     setTodo(e.target.value);
+//   };
 
-  const getAddTodoForm = (e) => {
-    setTodo(e.target.value);
-  };
+//   const handleAddTodoAxios = () => {
+//     if (!todo) {
+//       alert("Todo를 입력해주세요");
+//       return;
+//     }
+//     addTodoMutate({
+//       id: Math.random(),
+//       todo: todo,
+//       status: false,
+//     });
+//   };
 
-  // const handleAddTodo = () => {
-  //   if (!todo) return;
-  //   appDispatch(fetchAddTodo(todo));
-  //   setTodo("");
-  // };
+//   return (
+//     <div>
+//       <h1>The Todo List Page</h1>
+//       <hr />
+//       <Box mb="20px">
+//         <input
+//           type="text"
+//           placeholder="Todo 입력"
+//           value={todo}
+//           onChange={getAddTodoForm}
+//         />
+//         <button onClick={handleAddTodoAxios}>Add Todo</button>
+//       </Box>
+//       <hr />
+//       <Todo />
+//     </div>
+//   );
+// };
 
-  // const addTodoLostAxios = async () => {
-  //   axios.post("http://localhost:3001/list", {
-  //     id: Math.random(),
-  //     todo: todo,
-  //     status: false,
-  //   });
-  // };
-
-  // const addTodo = useMutation(
-  //   (todo) => axios.post("http://localhost:3001/list", todo),
-  //   {
-  //     onSuccess: () => {
-  //       console.log("success!");
-  //       queryClient.invalidateQueries("todo");
-  //     },
-  //   }
-  // );
-
-  const handleAddTodoAxios = () => {
-    // addTodo.mutate({
-    //   id: Math.random(),
-    //   todo: todo,
-    //   status: false,
-    // });
-    if (!todo) {
-      alert("Todo를 입력해주세요");
-      return;
-    }
-    addTodoMutate({
-      id: Math.random(),
-      todo: todo,
-      status: false,
-    });
-  };
-
-  return (
-    <div>
-      <h1>The Todo List Page</h1>
-      <hr />
-      <Box mb="20px">
-        <input
-          type="text"
-          placeholder="Todo 입력"
-          value={todo}
-          onChange={getAddTodoForm}
-        />
-        <button onClick={handleAddTodoAxios}>Add Todo</button>
-      </Box>
-      <hr />
-      <Todo />
-    </div>
-  );
-};
-
-export default TodoListPage;
+// export default TodoListPage;
+export { default } from "../../containers/todo-list";
