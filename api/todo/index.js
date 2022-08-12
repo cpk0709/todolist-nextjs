d36f4bcfd2874b8d9a304db78ec3f726d75abaf0
cpk0709 <cpk0709@gmail.com>
2022-08-12 12:21:16 +0900

    getTodo API 추가

--- api/todo/index.js
+++ api/todo/index.js
@@ -1,5 +1,10 @@
 import { instance } from "../config";
 
+export const getTodo = async () => {
+  const { data } = await instance.get("http://localhost:3001/list");
+  return data;
+};
+
 export const postTodo = async (todo) => {
   const { data } = await instance.post("/list", todo);
   return data;
