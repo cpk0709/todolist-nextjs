967e5105185edbe42efd226dc6d6af6690f9c363
cpk0709 <cpk0709@gmail.com>
2022-08-12 16:23:27 +0900

    add color.js file

---
+++ utils/color.js
@@ -0,0 +1,11 @@
+export const ColorSchema = {
+  key: "#BBFC88",
+  key2: "#BBFC87",
+  bg: "#020D16",
+  bgSub: "#1B252D",
+  text1: "#FFFFFF",
+  text2: "#D2D2D2",
+  text3: "#646464",
+  text4: "#999999",
+  error: "#EC464A",
+};
