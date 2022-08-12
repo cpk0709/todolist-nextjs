32c92dae94638d5abc3d11a899ac3e0c1863e76f
cpk0709 <cpk0709@gmail.com>
2022-08-12 14:37:15 +0900

    useMutation hook 추가

--- api/todo/todo-hooks.js
+++ api/todo/todo-hooks.js
@@ -4,3 +4,12 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 export const useQueryTodo = () => {
   return useQuery(["todo"], getTodo);
 };
+
+export const useMutateAddTodo = () => {
+  return useMutation(postTodo, {
+    onSuccess: () => {
+      console.log("success!");
+      queryClient.invalidateQueries("todo");
+    },
+  });
+};
